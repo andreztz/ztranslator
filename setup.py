fbaba8c812d5483da0fd633c16cc8bf47f49d532
andreztz <andreztz@gmail.com>
2018-08-17 20:54:04 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='ztranslate',
+    version='0.0.1',
+    description='translator.',
+    long_description=readme(),
+    keywords='translator tradutor',
+    platforms=['Linux'],
+    author='andreztz',
+    author_email='andreztz@gmail.com',
+    url='andreztz.github.io',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['requests']
+)
---
+++ translator/__init__.py
@@ -0,0 +1,3 @@
+from translate import Translator
+
+__all__ = [Translator]
