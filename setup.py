62bc60e9e233d5572ae4d2ee5b10165276500f7f
andreztz <andreztz@gmail.com>
2018-09-03 21:45:10 -0300

    changed version - 0.0.3

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 from setuptools import setup
 from setuptools import find_packages
 
+
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
+
 setup(
-    name='ztranslate',
-    version='0.0.1',
-    description='translator.',
+    name="ztranslator",
+    version="0.0.3",
+    description="powerful command line translator.",
     long_description=readme(),
-    keywords='translator tradutor',
-    platforms=['Linux'],
-    author='andreztz',
-    author_email='andreztz@gmail.com',
-    url='andreztz.github.io',
-    license='MIT',
+    long_description_content_type="text/markdown",
+    keywords="translator tradutor",
+    author="André P. Santos",
+    author_email="andreztz@gmail.com",
+    url="https://github.com/andreztz/ztranslator",
+    license="MIT",
     packages=find_packages(),
-    install_requires=['requests'],
-    entry_points={'console_scripts': ['ztranslator=translator.__main__:main']}
+    install_requires=["requests"],
+    entry_points={"console_scripts": ["ztranslator=translator.__main__:main"]},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ],
 )
