a1f65f4a037509f2fd883c8a6d94f93e52ad6007
andreztz <andreztz@gmail.com>
2018-10-13 21:47:09 -0300

    added main script

--- translator/__main__.py
+++ translator/__main__.py
@@ -1,3 +1,29 @@
-from .scripts import main
+#!/usr/bin/env python3
 
-main()
+import argparse
+import locale
+
+from translator import clip
+from notify import Notification
+
+from translator.translate import Translator
+
+default_locale = locale.getdefaultlocale()[0]
+to_lang = default_locale.split(".")[0].lower().replace("_", "-")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lang", nargs="?", default=to_lang, help="to lang")
+    parser.add_argument("--notify", nargs="?", const=True, help="notify")
+    parser.add_argument("--text", help="text")
+    args = parser.parse_args()
+
+    text = args.text if args.text else clip.get()
+
+    translator = Translator(to_lang=args.lang)
+    translated = translator.translate(text)
+    if args.notify:
+        Notification("Translator", translated).notify()
+    else:
+        print(translated)
