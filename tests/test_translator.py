94d71b8441fca536a1bdd9a8e8c10fbbdc0ab4de
André P. Santos <andreztz@gmail.com>
2021-11-23 13:04:12 -0300

    Adiciona exceção customizada

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -1,5 +1,8 @@
 import pytest
 from translator import Translator
+from translator.translate import SourceNotFound
+from translator.translate import SourceManager
+from translator.translate import TranslatorBase
 
 
 def test_translator_hello_world():
@@ -9,10 +12,13 @@ def test_translator_hello_world():
 
 def test_translator_source_not_found():
 
-    with pytest.raises(Exception) as excinfo:
+    with pytest.raises(SourceNotFound) as excinfo:
         Translator(from_lang="en", to_lang="pt", source="googles")
 
     error_message = "The source does not exist. Try `google` or `mymemory`."
     assert error_message in str(excinfo.value)
 
 
+def test_source_manager():
+    source = SourceManager('google').get(from_lang='pt', to_lang="eng")
+    assert isinstance(source, TranslatorBase)
