291af6d65ab12078806f867eb470c8d92d24d20f
André P. Santos <andreztz@gmail.com>
2021-11-27 22:08:29 -0300

    Renomeia parametros

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -6,19 +6,19 @@ from translator.translate import TranslatorBase
 
 
 def test_translator_hello_world():
-    translator = Translator(from_lang="en", to_lang="pt", source="google")
-    assert translator.translate('Hello World') == 'Olá Mundo'
+    translator = Translator(source_lang="en", target_lang="pt", source_api="google")
+    assert translator.translate("Hello World") == "Olá Mundo"
 
 
-def test_translator_source_not_found():
+def test_translator_source_api_not_found():
 
     with pytest.raises(SourceNotFound) as excinfo:
-        Translator(from_lang="en", to_lang="pt", source="googles")
+        Translator(source_lang="en", target_lang="pt", source_api="googles")
 
-    error_message = "The source does not exist. Try `google` or `mymemory`."
+    error_message = "The source API does not exist. Try `google` or `mymemory`."
     assert error_message in str(excinfo.value)
 
 
-def test_source_manager():
-    source = SourceManager('google').get(from_lang='pt', to_lang="eng")
+def test_api_manager():
+    source = SourceManager("google").get(source_lang="pt", target_lang="eng")
     assert isinstance(source, TranslatorBase)
