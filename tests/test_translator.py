1c249abecd2b6b3f1530c8572cf761a1fb7def5c
André P. Santos <andreztz@gmail.com>
2023-03-15 06:23:54 -0300

    Modifica referências de 'source' para 'provider'

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -1,28 +1,26 @@
 import pytest
 from translator import Translator
-from translator.translate import SourceNotFound
-from translator.translate import SourceManager
-from translator.translate import TranslatorBase
+from translator.translate import ProviderNotFound
+from translator.translate import ProviderManager
+from translator.translate import ProviderBase
 
 
 def test_translator_hello_world():
     translator = Translator(
-        source_lang="en", target_lang="pt", source_api="google"
+        source_lang="en", target_lang="pt", provider_api="google"
     )
     assert translator.translate("Hello World") == "Olá Mundo"
 
 
-def test_translator_source_api_not_found():
+def test_translator_provider_api_not_found():
 
-    with pytest.raises(SourceNotFound) as excinfo:
-        Translator(source_lang="en", target_lang="pt", source_api="googles")
+    with pytest.raises(ProviderNotFound) as excinfo:
+        Translator(source_lang="en", target_lang="pt", provider_api="googles")
 
-    error_message = (
-        "The source API does not exist. Try `google` or `mymemory`."
-    )
+    error_message = "The provider does not exist. Try `google` or `mymemory`."
     assert error_message in str(excinfo.value)
 
 
 def test_api_manager():
-    source = SourceManager("google").get(source_lang="pt", target_lang="eng")
-    assert isinstance(source, TranslatorBase)
+    source = ProviderManager("google").get(source_lang="pt", target_lang="eng")
+    assert isinstance(source, ProviderBase)
